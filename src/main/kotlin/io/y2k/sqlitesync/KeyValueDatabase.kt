844ddb586da8873b859bbfd2ef4f0a3c826df32c
y2k <itwisterlx@gmail.com>
2019-09-10 21:05:23 +0300

    add android db provider

--- src/main/kotlin/io/y2k/sqlitesync/KeyValueDatabase.kt
+++ src/main/kotlin/io/y2k/sqlitesync/KeyValueDatabase.kt
@@ -1,5 +1,6 @@
 package io.y2k.sqlitesync
 
+import android.database.sqlite.SQLiteDatabase
 import kotlinx.atomicfu.atomic
 import kotlinx.atomicfu.update
 import kotlinx.collections.immutable.PersistentMap
@@ -9,6 +10,38 @@ import kotlinx.collections.immutable.persistentListOf
 sealed class Command<out T> {
     class Update<T>(val key: String, val value: T) : Command<T>()
     class Remove(val key: String) : Command<Nothing>()
+}
+
+class AndroidDatabaseProvider(private val name: String, private val db: SQLiteDatabase) : DatabaseProvider {
+
+    init {
+        db.execSQL("CREATE TABLE $name (key TEXT, value TEXT, action TEXT)")
+    }
+
+    override suspend fun useTransaction(f: () -> Unit) {
+        db.beginTransaction()
+        try {
+            f()
+            db.setTransactionSuccessful()
+        } finally {
+            db.endTransaction()
+        }
+    }
+
+    override suspend fun <T : Any> query(sql: String, mapTo: (Row) -> T): List<T> {
+        return db.rawQuery(sql, emptyArray()).use { c ->
+            List(c.count) {
+                c.moveToPosition(it)
+                mapTo(object : Row {
+                    override fun getString(column: String): String? = c.getString(c.getColumnIndex(column))
+                    override fun getBoolean(column: String): Boolean = TODO()
+                    override fun getLong(column: String): Long = TODO()
+                })
+            }
+        }
+    }
+
+    override fun execSql(sql: String, vararg data: String) = db.execSQL(sql, data)
 }
 
 interface DatabaseProvider {
