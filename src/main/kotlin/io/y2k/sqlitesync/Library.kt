b77600ff31c6c6218dab6771e2856190144f505c
y2k <itwisterlx@gmail.com>
2019-09-10 12:21:56 +0300

    add key-value

--- src/main/kotlin/io/y2k/sqlitesync/Library.kt
+++ src/main/kotlin/io/y2k/sqlitesync/Library.kt
@@ -2,76 +2,116 @@ package io.y2k.sqlitesync
 
 import kotlinx.atomicfu.atomic
 import kotlinx.atomicfu.update
-import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.PersistentMap
+import kotlinx.collections.immutable.persistentHashMapOf
 import kotlinx.collections.immutable.persistentListOf
-import kotlinx.coroutines.delay
 
 sealed class Command<T> {
-    class Add<T>(val value: T) : Command<T>()
-    class Remove<T>(val value: T) : Command<T>()
-    class Update<T>(val value: T) : Command<T>()
+    class Add<T>(val key: String, val value: T) : Command<T>()
+    class Remove<T>(val key: String) : Command<T>()
+    class Update<T>(val key: String, val value: T) : Command<T>()
 }
 
 interface SQLiteDatabase {
     suspend fun useTransaction(f: () -> Unit)
     suspend fun <T : Any> query(sql: String, mapTo: (Row) -> T): List<T>
+    fun execSql(sql: String, vararg data: String)
 }
 
 interface Row {
-    fun getString(column: String): String
+    fun getString(column: String): String?
     fun getBoolean(column: String): Boolean
     fun getLong(column: String): Long
 }
 
-class Library<T : Any>(private val db: SQLiteDatabase, private val mapTo: (Row) -> T, private val table: String) {
+class Library<T : Any>(
+    private val db: SQLiteDatabase,
+    private val table: String,
+    private val serialize: (T) -> String,
+    private val deserialize: (String) -> T
+) {
 
     private val log = atomic(persistentListOf<Command<T>>())
-    private val store = atomic(persistentListOf<T>())
+    private val store = atomic(persistentHashMapOf<String, T>())
 
-    suspend fun init() {
-        db.query("SELECT * FROM $table") { mapTo(it) }
-            .forEach { x -> log.update { it.add(Command.Add(x)) } }
+    private suspend fun init() {
+        db.query("SELECT * FROM $table") {
+            Record(it.getString("action")!!, it.getString("key")!!, it.getString("value"))
+        }.forEach {
+            log.update { xs ->
+                xs.add(
+                    when (it.action) {
+                        "insert" -> Command.Add(it.key, deserialize(it.value!!))
+                        "update" -> Command.Update(it.key, deserialize(it.value!!))
+                        "delete" -> Command.Remove(it.key)
+                        else -> error("$it")
+                    }
+                )
+            }
+        }
         reloadStore()
     }
 
-    suspend fun update(f: (PersistentList<T>) -> List<Command<T>>) {
+    suspend fun update(f: (PersistentMap<String, T>) -> List<Command<T>>) {
         f(store.value).forEach { x ->
             log.update { it.add(x) }
         }
-
         reloadStore()
         reloadDatabase()
     }
 
+    var currentOffset = 0
+
     private suspend fun reloadDatabase() {
-        var currentOffset = 0
-        while (true) {
-            val snapshot = log.value
-            db.useTransaction {
-                snapshot.drop(currentOffset).forEach {
-                    when (it) {
-                        is Command.Add -> TODO("INSERT ... INTO words ...")
-                        is Command.Remove -> TODO("DELETE FROM words")
-                        is Command.Update -> TODO("UPDATE")
-                    }
+        val snapshot = log.value
+        db.useTransaction {
+            snapshot.drop(currentOffset).forEach {
+                when (it) {
+                    is Command.Add ->
+                        db.execSql(
+                            "INSERT INTO $table (action, key, value) VALUES('insert', ?, ?)",
+                            it.key, serialize(it.value))
+                    is Command.Remove ->
+                        db.execSql(
+                            "INSERT INTO $table (action, key) VALUES('delete', ?)",
+                            it.key)
+                    is Command.Update ->
+                        db.execSql(
+                            "INSERT INTO $table (action, key, value) VALUES('update', ?, ?)",
+                            it.key, serialize(it.value))
                 }
-                currentOffset = snapshot.size
             }
-            delay(100)
+            currentOffset = snapshot.size
         }
     }
 
+    var currentStoreOffset = 0
+
     private fun reloadStore() {
-        store.value = log.value.asSequence().fold(persistentListOf(),
+        val snapshot = log.value
+        store.value = snapshot.drop(currentStoreOffset).asSequence().fold(persistentHashMapOf(),
             { xs, x ->
                 when (x) {
-                    is Command.Add -> xs.add(x.value)
-                    is Command.Remove -> TODO()
-                    is Command.Update -> TODO()
+                    is Command.Add -> xs.put(x.key, x.value)
+                    is Command.Remove -> xs.remove(x.key)
+                    is Command.Update -> xs.put(x.key, x.value)
                 }
             }
         )
+        currentStoreOffset = snapshot.size
     }
 
-    fun snapshot(): PersistentList<T> = store.value
+    val snapshot get() = store.value
+
+    private data class Record(val action: String, val key: String, val value: String?)
+
+    companion object {
+        suspend fun <T : Any> make(
+            db: SQLiteDatabase, table: String, serialize: (T) -> String, deserialize: (String) -> T) =
+            run {
+                val l = Library(db, table, serialize, deserialize)
+                l.init()
+                l
+            }
+    }
 }
--- src/test/kotlin/io/y2k/sqlitesync/LibraryTests.kt
+++ src/test/kotlin/io/y2k/sqlitesync/LibraryTests.kt
@@ -11,62 +11,30 @@ class LibraryTests {
 
     @Test(timeout = 5_000)
     fun test() = runBlocking {
-        val db = object : SQLiteDatabase {
+        val db = TestDatabase()
 
-            private val conn: Connection
+        var lib = makeLibrary(db)
 
-            init {
-                Class.forName("org.sqlite.JDBC")
-                conn = DriverManager.getConnection("jdbc:sqlite::memory:")
-                conn.createStatement().execute("CREATE TABLE foo (favorite BOOL, source TEXT, text target, id NUMBER)")
-            }
+        lib.update { List(3) { Command.Add("1", Item(false, "1s", "1t", 1L)) } }
+        assertEquals(1, lib.snapshot.size)
 
-            override suspend fun useTransaction(f: () -> Unit): Unit = f()
+        lib = makeLibrary(db)
+        assertEquals(1, lib.snapshot.size)
 
-            override suspend fun <T : Any> query(sql: String, mapTo: (Row) -> T): List<T> {
-                val stmt = conn.createStatement()
-                val resultSet = stmt.executeQuery(sql)
-                if (resultSet.isClosed) return emptyList()
-                return resultSet
-                    .toList { row ->
-                        mapTo(object : Row {
-                            override fun getBoolean(column: String): Boolean = row.getBoolean(column)
-                            override fun getLong(column: String): Long = row.getLong(column)
-                            override fun getString(column: String): String = row.getString(column)
-                        })
-                    }
-            }
+        lib.update { listOf(Command.Remove(it.toList().first().first)) }
+        lib = makeLibrary(db)
+        assertEquals(0, lib.snapshot.size)
+    }
 
-            fun <T : Any> ResultSet.toList(f: (ResultSet) -> T): List<T> =
-                use {
-                    val r = ArrayList<T>()
-                    while (it.next()) r.add(f(it))
-                    r
-                }
-        }
-
-        val l = Library(
-            db,
+    private suspend fun makeLibrary(db: SQLiteDatabase): Library<Item> {
+        return Library.make(
+            db, "history",
+            { "${it.favorite}|${it.source}|${it.target}|${it.id}" },
             {
-                Item(
-                    it.getBoolean("favorite"),
-                    it.getString("source"),
-                    it.getString("target"),
-                    it.getLong("id")
-                )
-            },
-            "foo"
+                val (a, b, c, d) = it.split('|')
+                Item(a.toBoolean(), b, c, d.toLong())
+            }
         )
-
-        l.init()
-
-        l.update {
-            listOf(Command.Add(Item(false, "1s", "1t", 1L)))
-        }
-
-        val snap = l.snapshot()
-
-        assertEquals(1, snap.size)
     }
 
     data class Item(
@@ -75,4 +43,48 @@ class LibraryTests {
         val target: String,
         val id: Long
     )
+}
+
+private class TestDatabase : SQLiteDatabase {
+
+    private val conn: Connection
+
+    init {
+        Class.forName("org.sqlite.JDBC")
+        conn = DriverManager.getConnection("jdbc:sqlite::memory:")
+        conn.createStatement().execute("CREATE TABLE history (key TEXT, value TEXT, action TEXT)")
+    }
+
+    override fun execSql(sql: String, vararg data: String) {
+        val stat = conn.prepareStatement(sql)
+        data.forEachIndexed { index, value ->
+            stat.setString(index + 1, value)
+        }
+
+        stat.execute()
+        stat.close()
+    }
+
+    override suspend fun useTransaction(f: () -> Unit): Unit = f()
+
+    override suspend fun <T : Any> query(sql: String, mapTo: (Row) -> T): List<T> {
+        val stmt = conn.createStatement()
+        val resultSet = stmt.executeQuery(sql)
+        if (resultSet.isClosed) return emptyList()
+        return resultSet
+            .toList { row ->
+                mapTo(object : Row {
+                    override fun getBoolean(column: String): Boolean = row.getBoolean(column)
+                    override fun getLong(column: String): Long = row.getLong(column)
+                    override fun getString(column: String): String? = row.getString(column)
+                })
+            }
+    }
+
+    fun <T : Any> ResultSet.toList(f: (ResultSet) -> T): List<T> =
+        use {
+            val r = ArrayList<T>()
+            while (it.next()) r.add(f(it))
+            r
+        }
 }
